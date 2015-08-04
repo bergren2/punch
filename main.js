4b5fef1083a7efac7f6f0670ed6e1ad012eb3a9a
Dan Bergren <bergren2@gmail.com>
2015-08-03 19:50:58 -0500

    add default Punch Brothers video :violin:

--- main.js
+++ main.js
@@ -21,7 +21,12 @@ app.on('window-all-closed', function () {
 // initialization and is ready to create browser windows.
 app.on('ready', function () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({
+    'width': 640,
+    'height': 390,
+    'resizable': false,
+    'use-content-size': true
+  });
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
